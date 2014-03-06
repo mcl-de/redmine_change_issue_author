e89edbd0646f4ed64ca8f839540d31ac6c93a15c
ngyuki <ngyuki.ts@gmail.com>
2014-03-06 17:39:55 +0900

    Ignore group principal

--- lib/issue_patch.rb
+++ lib/issue_patch.rb
@@ -6,6 +6,11 @@ module ChangeAuehorIssuePatch
     base.class_eval do
       safe_attributes 'author_id',
 		:if => lambda {|issue, user| (user.allowed_to?(:edit_issue_author, issue.project))}
+
+      def assignable_users_without_group
+        assignable_users.select {|user| !user.has_attribute?(:type) || user.type == "User" }
+      end
+
     end
 
   end
