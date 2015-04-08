d41d8e524de008faa2f05ce5b7abadd3815892ec
eskin <es@drm.su>
2015-04-08 22:10:50 +0300

    Some refactoring

--- lib/issue_patch.rb
+++ lib/issue_patch.rb
@@ -5,9 +5,9 @@ module ChangeAuehorIssuePatch
 
     base.class_eval do
       safe_attributes 'author_id',
-		:if => lambda {|issue, user| (user.allowed_to?(:edit_issue_author, issue.project))}
+        :if => lambda {|issue, user| (user.allowed_to?(:edit_issue_author, issue.project))}
 
-      def assignable_users_without_group
+      def all_users
         User.active.uniq
       end
 
